b40004dca1693dda70c420ce7714c2666ea3e2d1
Gowtham <gowthampannem2007@gmail.com>
2026-05-27 20:31:00 +0530

    added generate button to generate a strong password.

--- Strong Password Generator project/script.js
+++ Strong Password Generator project/script.js
@@ -31,3 +31,36 @@ function isStrongOrNot()
     else
         display.innerHTML = 'Weak';
 }
+
+let generateButton = document.querySelector('button');
+
+generateButton.addEventListener('click',() => {generateStrongPassword();});
+
+function generateStrongPassword()
+{
+    let result = '';
+
+    const small = 'abcdefghijklmnopqrstuvwxyz';
+    const capital = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numbers = '0123456789';
+    const special = '!@#$%&?^';
+
+    // add at least one character from each type
+    result += small[Math.floor(Math.random() * small.length)];
+    result += capital[Math.floor(Math.random() * capital.length)];
+    result += numbers[Math.floor(Math.random() * numbers.length)];
+    result += special[Math.floor(Math.random() * special.length)];
+
+    // all possible characters
+    const all = small + capital + numbers + special;
+
+    // generate remaining characters
+    for(let i = 0; i < 4; i++)
+    {
+        result += all[Math.floor(Math.random() * all.length)];
+    }
+
+    pass.value = result;
+
+    isStrongOrNot();
+}
